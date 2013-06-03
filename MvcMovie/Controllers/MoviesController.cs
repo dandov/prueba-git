ac7629b1ed88e0d57cada35d56b9c52de324e5c0
dandov <dandov@hotmail.com>
2013-06-03 14:02:24 -0500

    primer commit

---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+using MvcMovie.Service;
+
+namespace MvcMovie.Controllers
+{ 
+    public class MoviesController : Controller
+    {
+        private MovieDBContext db = new MovieDBContext();
+        //
+        // GET: /Movies/
+
+        public ViewResult Index()
+        {
+            return View(MovieService.Index());
+        }
+
+        //
+        // GET: /Movies/Details/5
+
+        public ViewResult Details(int id)
+        {
+            return View(MovieService.Find(id));
+        }
+
+        //
+        // GET: /Movies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Movies/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                MovieService.CrearMovie(movie);
+                return RedirectToAction("Index");  
+            }
+
+            return View(movie);
+        }
+        
+        //
+        // GET: /Movies/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Movie movie = MovieService.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                MovieService.EditMovie(movie);
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: /Movies/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Movie movie = MovieService.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        //
+        // POST: /Movies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id = 0)
+        {
+            Movie movie = MovieService.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            MovieService.DeleteMovie(movie);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult SearchIndex(string movieGenre, string searchString)
+        {
+            ViewBag.movieGenre = new SelectList(MovieService.GetGendres());
+            return View(MovieService.SearchMovie(movieGenre, searchString));
+        }
+    }
+}
---
+++ MvcMovie/Service/MovieService.cs
@@ -0,0 +1,86 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Service
+{
+    public class MovieService
+    {
+        private static MovieDBContext db = new MovieDBContext();
+
+        public static List<Movie> Index()
+        {
+            return db.Movies.ToList();
+        }
+
+        public static Movie Find(int id)
+        {
+            return db.Movies.Find(id);
+        }
+
+        public static void CrearMovie(Movie movie)
+        {
+            db.Movies.Add(movie);
+            db.SaveChanges();
+        }
+
+        public static void EditMovie(Movie movie)
+        {
+            Movie m = Find(movie.ID);
+            CopyMovie(m, movie);
+            db.SaveChanges();
+        }
+
+        private static void CopyMovie(Movie to, Movie from)
+        {
+            if (to!=null && from!=null)
+            {
+                to.Price = from.Price;
+                to.Rating = from.Rating;
+                to.ReleaseDate = from.ReleaseDate;
+                to.Title = from.Title;
+                to.Genre = from.Genre;
+            }
+        }
+
+        public static void DeleteMovie(Movie movie)
+        {
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+        }
+
+        public static List<string> GetGendres()
+        {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+
+            return GenreLst;
+        }
+
+        public static IQueryable<Movie> SearchMovie(string movieGenre, string searchString)
+        {
+            IQueryable<Movie> movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return movies;
+        }
+    }
+}
